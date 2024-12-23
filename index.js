ed0df71c124a9af190d956207901aaa95c0ba447
amir <amir.sisi.cobra11.com@gmail.com>
2024-12-23 17:40:18 +0330

    install socket.io

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const { createServer } = require("node:http");
+const { createServer } = require("http");
 const { join } = require("path")
+const { Server} = require("socket.io");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "/index.html"));
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
 });
 
 server.listen(3000, () => {
